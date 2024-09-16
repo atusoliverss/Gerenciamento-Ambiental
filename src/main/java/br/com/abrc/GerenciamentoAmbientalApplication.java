163f88ed19c03e6b1a93379984ce53664287b81a
atusoliverss <batistaatus131@gmail.com>
2024-09-16 09:19:51 -0300

    first commit

---
+++ src/main/java/br/com/abrc/GerenciamentoAmbientalApplication.java
@@ -0,0 +1,13 @@
+package br.com.abrc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciamentoAmbientalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciamentoAmbientalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/abrc/GerenciamentoAmbientalApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.abrc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciamentoAmbientalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
