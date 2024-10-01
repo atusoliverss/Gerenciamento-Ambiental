2a3463dad17e9dbf8b9b28f1c4db2e63f5878aed
Rian <rian10cardosi@gmail.com>
2024-09-30 23:11:47 -0300

    Adição da entidade "User"

---
+++ src/main/java/br/com/abrc/user/entity/User.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.abrc.user.entity;
+
+/**
+ *
+ * @author rian1
+ */
+public class User {
+    private String name;
+    private String email;
+    
+}
